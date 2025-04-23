08234e6d6c0cd13e1da976004df7de27c3725ae1
notmario <59957544+notmario@users.noreply.github.com>
2025-04-23 19:07:39 +0800

    forgot this one

--- jokers/farmmerge.lua
+++ jokers/farmmerge.lua
@@ -61,8 +61,17 @@ local joker = {
         last_card.ability[k] = new_stats[k]
       end
 
+      local destroyed_cards = {}
+
       for _, v in pairs(context.full_hand) do
-        if v ~= last_card then v:start_dissolve({HEX("57ecab")}, nil, 1.6) end
+        if v ~= last_card then 
+          destroyed_cards[#destroyed_cards + 1] = v
+          v:start_dissolve({HEX("57ecab")}, nil, 1.6)
+        end
+      end
+      
+      for i = 1, #G.jokers.cards do
+        G.jokers.cards[i]:calculate_joker({remove_playing_cards = true, removed = destroyed_cards})
       end
 
       return {
