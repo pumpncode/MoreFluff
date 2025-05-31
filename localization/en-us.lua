b850d9d06cd7e0c30159e94dda2e0e5331ff938b
notmario <59957544+notmario@users.noreply.github.com>
2025-05-31 19:18:27 +0800

    violetposting today

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1041,6 +1041,16 @@ local loc_stuff = {
           art_credit("Multi")
         },
       },
+      c_mf_violet = {
+        name = "Violet",
+        text = {
+          "Create a {C:dark_edition}Negative{} {C:spectral}Finity{}",
+          "card for every {C:attention}#4#{} rounds",
+          "this has been held",
+          "{C:inactive}(Currently {C:attention}#1#{C:inactive}, {}[{C:attention}#2#{C:inactive}#3#{}]{C:inactive})",
+          art_credit("Multi")
+        },
+      },
     },
     Rotarot = {
       c_mf_rot_fool = {
